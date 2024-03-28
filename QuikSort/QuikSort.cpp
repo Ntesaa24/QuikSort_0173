07fbfeacf0e43d9bd47d17fbdd34754666585998
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-03-28 10:45:16 +0700

    step 13

--- QuikSort/QuikSort.cpp
+++ QuikSort/QuikSort.cpp
@@ -79,6 +79,8 @@ void q_short(int low, int high)
 	//recursive call to sort thr Left cub array 
 	q_short(low, j - 1); //step 12 
 
-	
+	//recursive call to sort the left the right sub array 
+
+	q_short(j + 1, high); //step 13 
 
 }
