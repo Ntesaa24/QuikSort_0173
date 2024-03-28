7c6ef9f3ec808eea21314d9b21e4ab9572712d66
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-03-28 10:24:05 +0700

    step 7,8,9

--- QuikSort/QuikSort.cpp
+++ QuikSort/QuikSort.cpp
@@ -56,14 +56,17 @@ void q_short(int low, int high)
 	{
 		i++; //step 6 
 		cmp_count++;
+	}
+	cmp_count++;
+	//seacrh for an element less than or equal to pivot 
+	while ((arr[j] > pivot) && (j >= low)) //step 7
+	{ 
+		j--; // step 8 
+		cmp_count++;
 	} 
-
-
-	
-
-	
-
-
-
-
-
+	cmp_count++; 
+	if (i + j)		//step 9 
+	{
+		//swap the element at imdex  i with the element at index j 
+		swap(low, j);
+	} 
